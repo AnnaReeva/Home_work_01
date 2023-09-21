95b01e3a8bb9e78b2a9dfb4cc657d9a6a18b5af3
Anna Reeva <annree@yandex.ru>
2023-09-21 12:55:21 +0300

    add Task_34

--- Home_work_01/Task_34/Program.cs
+++ Home_work_01/Task_34/Program.cs
@@ -2,6 +2,45 @@
 положительными трёхзначными числами. Напишите
 программу, которая покажет количество чётных чисел в
 массиве.
-[345, 897, 568, 234] -> 2  */
+[345, 897, 568, 234] -> 2 */
+
+int[] GetArray(int Length, int minValue = 100, int maxValue = 999)  
+
+{
+    int[] array = new int[Length];                             
+
+        for (int i = 0; i < array.Length; i++)
+        {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+        }
+        return array;
+}
 
 
+void PrintArray(int[] array)
+{
+    foreach (var item in array)    
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}                                       
+
+int CountEvenNumber(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+
+}
+
+int[] massive = GetArray(8);
+PrintArray(massive);   
+
+Console.WriteLine(CountEvenNumber(massive));
