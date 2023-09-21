18b9d33e20472da395ec0398116619acd9ed88b7
Anna Reeva <annree@yandex.ru>
2023-09-21 12:57:58 +0300

    add Task_36

---
+++ Home_work_01/Task_36/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный
+случайными числами. Найдите сумму элементов, стоящих
+на нечётных позициях.
+[3, 7, 23, 12] -> 26
+[-4, -6, 89, 6] -> 85   */
+
+int[] GetArray(int Length, int minValue = -20, int maxValue = 20)  
+
+{
+    int[] array = new int[Length];                             
+
+        for (int i = 0; i < array.Length; i++)
+        {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+        }
+        return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)    
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}                                       
+
+
+int SumOdd (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+            sum = sum + array[i];
+    }
+    return sum;
+
+}
+
+int[] massive = GetArray(8);
+PrintArray(massive);
+Console.WriteLine(SumOdd(massive));
