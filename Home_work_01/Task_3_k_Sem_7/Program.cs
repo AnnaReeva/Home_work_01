98e96c5b39b83c2f233e16cce6f0fffff7219eaa
Anna Reeva <annree@yandex.ru>
2023-10-02 08:13:30 +0300

    add task_54

---
+++ Home_work_01/Task_3_k_Sem_7/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    int[,] matrix = new int[n, m];
+    int current_number = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        { 
+           matrix[i, j] = current_number; 
+           current_number += k;
+        }
+        
+    }
+    
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] matrix = CreateIncreasingMatrix(3, 4, 2);
+PrintArray(matrix);
+
+double [] FindAverageInColumns (int [,] matrix)
+{
+    double[] = new double[n];
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[j.i];
+        }
+        
+    }
+
+
+}
---
+++ Home_work_01/Task_54/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая
+упорядочит по убыванию элементы каждой строки двумерного массива */
+
+
+int[,] GetArray(int rows, int cols, int minValue = 1, int maxValue = 10)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1) ;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] OrganizeRowFromMaxToMin(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   
+    for (int k = 0; k < array.GetLength(1) -1; k++)  
+        for (int j = 0; j < array.GetLength(1) -1; j++)
+        {
+            if (array[i, j] < array[i, j +1])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, j +1];
+                array[i, j +1] = temp;
+            }
+            
+        }
+       
+    }
+    return array;
+}
+
+int [,] array = GetArray(4, 6);
+PrintArray(array);
+
+int[,] resArray = OrganizeRowFromMaxToMin(array);
+
+PrintArray(resArray);
